c87eb82bfbf4e8f54aaa595597f3bcf0c300300d
Sridhar Anandakrishnan <sak@essc.psu.edu>
2019-06-14 22:03:43 -0400

    initial commit

---
+++ seg2/seg2.go
@@ -0,0 +1,106 @@
+package seg2
+
+import (
+	"bytes"
+	"encoding/binary"
+	"log"
+	"os"
+	"strings"
+)
+
+var HDRS = []string{
+	"RECEIVER_LOCATION",    // int
+	"SAMPLE_INTERVAL",      // float
+	"SOURCE_LOCATION",      // int
+	"SHOT_SEQUENCE_NUMBER", // int
+	"DELAY",                // float
+}
+
+type Seg2TrcHdr map[string][]string
+
+type SEG2Trace struct {
+	Data []float32
+	Hdr  Seg2TrcHdr
+}
+type FileHeader struct {
+	FDID        uint16
+	RevNum      uint16
+	TrcPtrLen   uint16
+	NumTrcs     uint16
+	StrTermLen  uint8
+	StrTerm0    byte
+	StrTerm1    byte
+	LineTermLen uint8
+	LineTerm0   byte
+	LineTerm1   byte
+	Res         [18]byte
+	//Strings     []byte
+}
+
+// TrcBlkHdr is th
+type TrcBlkHdr struct {
+	TrcID      uint16
+	BlkSiz     uint16
+	DataBlkSiz uint32
+	NSamps     uint32
+	DataFormat uint8
+	Res        [19]byte
+}
+
+// ReadSEG2 reads a SEG2 formatted file and returns an array of `SEG2Trace`
+func ReadSEG2(fn string) []SEG2Trace {
+	// seg2 file header
+	fh := &FileHeader{}
+	f, err := os.Open(fn)
+	if err != nil {
+		log.Fatal("can't open", fn)
+	}
+	defer f.Close()
+
+	// read file header block
+	binary.Read(f, binary.LittleEndian, fh)
+	//fmt.Printf("%+v\n", fh)
+
+	// read trace pointer block
+	trcptrs := make([]uint32, fh.NumTrcs)
+	binary.Read(f, binary.LittleEndian, &trcptrs)
+
+	// read trace header block and data block
+	var seg2trcs = make([]SEG2Trace, fh.NumTrcs)
+	for i, ptr := range trcptrs {
+		//fmt.Println("trace", i)
+		f.Seek(int64(ptr), os.SEEK_SET)
+		tbh := &TrcBlkHdr{}
+		binary.Read(f, binary.LittleEndian, tbh)
+		//fmt.Println(tbh)
+		//trcStrLen := tbh.BlkSiz - 32
+		//trcStr := make([]byte, trcStrLen)
+		var hdr = make(Seg2TrcHdr)
+		var strOffset uint16
+		for {
+			binary.Read(f, binary.LittleEndian, &strOffset)
+			//fmt.Println(strOffset)
+			if strOffset == 0 {
+				break
+			}
+			var str = make([]byte, strOffset-2)
+			binary.Read(f, binary.LittleEndian, &str)
+
+			// assuming only one string terminating char, get rid of it
+			str = bytes.Trim(str, string([]byte{fh.StrTerm0}))
+
+			//fmt.Println(string(str))
+			comps := strings.Split(string(str), " ")
+			if len(comps) > 0 {
+				hdr[comps[0]] = comps[1:]
+			}
+			//fmt.Println(hdr)
+		}
+		f.Seek(int64(ptr)+int64(tbh.BlkSiz), os.SEEK_SET)
+		var data = make([]float32, tbh.NSamps)
+		binary.Read(f, binary.LittleEndian, &data)
+		//fmt.Println(hdr, data[:10])
+		seg2trcs[i] = SEG2Trace{Data: data, Hdr: hdr}
+	}
+	return seg2trcs
+}
---
+++ segy/segy.go
@@ -0,0 +1,165 @@
+package segy
+
+import (
+	"bytes"
+	"encoding/binary"
+	"io"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/sanandak/seg/seg2"
+)
+
+const SEGY_TRACE_HDR_LEN = 240
+
+type TraceHeader struct {
+	Tracl                                                                int32
+	Tracr                                                                int32
+	Fldr                                                                 int32
+	Tracf                                                                int32
+	Ep                                                                   int32
+	CDP                                                                  int32
+	CDPT                                                                 int32
+	Trid                                                                 int16
+	Nvs                                                                  int16
+	Nhs                                                                  int16
+	Duse                                                                 int16
+	Offset                                                               int32
+	Gelev                                                                int32
+	Selev                                                                int32
+	Sdepth                                                               int32
+	Gdel                                                                 int32
+	Sdel                                                                 int32
+	SwDep                                                                int32
+	GwDep                                                                int32
+	Scalel                                                               int16
+	Scalco                                                               int16
+	Sx                                                                   int32
+	Sy                                                                   int32
+	Gx                                                                   int32
+	Gy                                                                   int32
+	CoUnit                                                               int16
+	WeVel                                                                int16
+	SweVel                                                               int16
+	Sut, Gut, Sstat, Gstat, Tstat, Laga, Lagb, Delrt, Muts, Mute         int16
+	Ns, Dt                                                               uint16
+	Gain, Igc, Igi, Corr, Sfs, Sfe, Slen, Styp, Stas, Stae, Tatyp        int16
+	Afilf, Afils, NoFilf, NoFils, Lcf, Hcf, Lcs, Hcs, Year, Day          int16
+	Hour, Minute, Sec, Timbas, Trwf, Grnors, Grnofr, Grnlof, Gaps, Otrav int16
+	D1, F1, D2, F2, Ungpow, Unscale                                      float32
+	Ntr                                                                  int32
+	Mark, Shortpad                                                       int16
+	Unass                                                                [14]int16 // unassigned short array
+}
+
+type Trace struct {
+	TraceHeader
+	Data []float32
+}
+
+func ReadSU(fn string) []Trace {
+	f, err := os.Open(fn)
+	if err != nil {
+		log.Fatal("can't open", fn)
+	}
+	defer f.Close()
+
+	trcs := make([]Trace, 0)
+	for {
+		hdr := TraceHeader{}
+		err := binary.Read(f, binary.BigEndian, &hdr)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal("err reading from ", fn)
+		}
+		ns := hdr.Ns
+		//dt := hdr.Dt
+		data := make([]float32, ns)
+		err = binary.Read(f, binary.LittleEndian, &data)
+		if err != nil {
+			log.Fatal("err reading data from", fn)
+		}
+		trcs = append(trcs, Trace{TraceHeader: hdr, Data: data})
+	}
+	return trcs
+}
+
+// WriteSU - to fn (string) write trcs ([]Trace)
+func WriteSU(fn string, trcs []Trace) int {
+	f, err := os.OpenFile(fn, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal("err opening out file", fn)
+	}
+	defer f.Close()
+
+	var buf []byte
+	b := bytes.NewBuffer(buf)
+
+	var nwritten int
+	for i, trc := range trcs {
+		binary.Write(b, binary.BigEndian, trc.TraceHeader)
+		binary.Write(b, binary.BigEndian, trc.Data)
+		n, err := f.Write(b.Bytes())
+		if err != nil {
+			log.Fatal("err writing trace #", i)
+		}
+		nwritten += n
+		b.Reset()
+	}
+	return nwritten
+}
+
+// SEG2SEGY returns a byte slice representing a SEG-Y trace
+// including 240 byte header and trace data
+func SEG2SEGY(seg2trc seg2.SEG2Trace) []byte {
+	trc := &Trace{}
+	hdr := &trc.TraceHeader
+	hdr.Ns = uint16(len(seg2trc.Data))
+	hdr.Dt = uint16(1000)
+	if dt, ok := seg2trc.Hdr["SAMPLE_INTERVAL"]; ok {
+		v, err := strconv.ParseFloat(dt[0], 32)
+		if err == nil {
+			hdr.Dt = uint16(v * 1.e6)
+		}
+	}
+
+	if s, ok := seg2trc.Hdr["SHOT_SEQUENCE_NUMBER"]; ok {
+		v, err := strconv.Atoi(s[0])
+		if err == nil {
+			hdr.Fldr = int32(v)
+		}
+	}
+
+	if s, ok := seg2trc.Hdr["RECEIVER_LOCATION"]; ok {
+		v, err := strconv.ParseFloat(s[0], 32)
+		if err == nil {
+			hdr.Gx = int32(v)
+		}
+	}
+
+	if s, ok := seg2trc.Hdr["SOURCE_LOCATION"]; ok {
+		v, err := strconv.ParseFloat(s[0], 32)
+		if err == nil {
+			hdr.Sx = int32(v)
+		}
+	}
+	if s, ok := seg2trc.Hdr["CHANNEL_NUMBER"]; ok {
+		v, err := strconv.Atoi(s[0])
+		if err == nil {
+			hdr.Tracr = int32(v)
+			hdr.Tracf = int32(v)
+		}
+	}
+
+	hdr.Offset = hdr.Sx - hdr.Gx
+	trc.Data = seg2trc.Data
+
+	var buf []byte
+	b := bytes.NewBuffer(buf)
+	binary.Write(b, binary.BigEndian, hdr)
+	binary.Write(b, binary.BigEndian, trc.Data)
+	return b.Bytes()
+}
